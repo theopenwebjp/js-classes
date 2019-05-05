94647274a67014e33b58133db167c6b41fcba7e3
Damien Golding <noreply@damiengolding.com>
2019-05-05 17:47:00 +0900

    format converter fixes

--- class/FormatConverter.js
+++ class/FormatConverter.js
@@ -1,155 +1,160 @@
+const ObjectTraverser = require('object-traverser')
 
 /**
  * Abstract class for converting anything.
  */
-var FormatConverter = function(formats, settings){
-  
-  var converter = {};
-  
-  //??Should make static.
+var FormatConverter = function (formats, settings) {
+  var converter = {}
+
+  // ??Should make static.
   converter.settings = {
     maxDepth: 5
-  };
-  converter.formats = [];
-  
-  converter.Format = function(){
+  }
+  converter.formats = []
+
+  converter.Format = function () {
     return {
-      id: "",
-      name: "",
-      extension: "",
+      id: '',
+      name: '',
+      extension: '',
       mimeType: {
-        main: "",
-        sub: ""
+        main: '',
+        sub: ''
       },
       converters: []
-    };
+    }
   }
-  
-  converter.Converter = function(){
+
+  converter.Converter = function () {
     return {
-      targetId: "",
-      handle: function(data){
-        return new Promise(function(resolve, reject){
-          //convert here
-          resolve(data);
-        });;
+      targetId: '',
+      handle: function (data) {
+        return new Promise(function (resolve, reject) {
+          // convert here
+          resolve(data)
+        })
       }
     }
   }
-  
-  converter.setup = function(formats, settings){
-    if(formats){
-      converter.formats = formats;
+
+  converter.setup = function (formats, settings) {
+    if (formats) {
+      converter.formats = formats
     }
-    
-    if(settings){
-      converter.settings = settings;
+
+    if (settings) {
+      converter.settings = settings
     }
   }
-  
-  converter.getFormat = function(id){
-    //Default lookup method
-    return converter.getFormatByAttribute("id", id);
+
+  converter.getFormat = function (id) {
+    // Default lookup method
+    return converter.getFormatByAttribute('id', id)
   }
-  
-  converter.getFormatByAttribute = function(attr, val){
-    var f = converter.formats;
-    for(var i=0; i<f.length; i++){
-      if(f[i][attr] === val){
-        return f[i];
+
+  converter.getFormatByAttribute = function (attr, val) {
+    var f = converter.formats
+    for (var i = 0; i < f.length; i++) {
+      if (f[i][attr] === val) {
+        return f[i]
       }
     }
-    
-    return null;
+
+    return null
   }
-  
-  converter.getFormatIds = function(){
-    return converter.getFormatAttributes("id");
+
+  converter.getFormatIds = function () {
+    return converter.getFormatAttributes('id')
   }
-  
-  converter.getFormatAttributes = function(attr){
-    var attributes = [];
-    
-    var f = converter.formats;
-    for(var i=0; i<f.length; i++){
-      attributes.push(f[i][attr]);
+
+  converter.getFormatAttributes = function (attr) {
+    var attributes = []
+
+    var f = converter.formats
+    for (var i = 0; i < f.length; i++) {
+      attributes.push(f[i][attr])
     }
-    
-    return attributes;
+
+    return attributes
   }
-  
-  converter.convert = function(data, fromId, toId, callback){
-    var path = converter.getBestConversionPath(fromId, toId);
-    //??id path to converters path
-    var promises = [];
-    for(var i=0; i<path.length; i++){
-      promises.push(converter.executeConverter(path[i]));
+
+  converter.convert = function (data, fromId, toId, callback) {
+    var path = converter.getBestConversionPath(fromId, toId)
+    // ??id path to converters path
+    var promises = []
+    for (var i = 0; i < path.length; i++) {
+      promises.push(converter.executeConverter(path[i]))
     }
-    
-    Promise.all(promises).then(function(convertedData){
-      callback(convertedData);
-    }).catch(err){
-      console.error(err);
-    }
+
+    Promise.all(promises).then(function (convertedData) {
+      callback(convertedData)
+    }).catch(err => {
+      console.error(err)
+    })
   }
-  
-  converter.executeConverter = function(converter){
-    return converter.handle();
+
+  converter.executeConverter = function (converter) {
+    return converter.handle()
   }
-  
-  converter.getBestConversionPath = function(fromId, toId){
-    var paths = converter.getConversionPaths(fromId, toId);
-    var chosenPath = null;
-    for(var i=0; i<paths.length; i++){
-      if(chosenPath === null || paths[i].length < chosenPath.length){
-        chosenPath = paths[i];
+
+  converter.getBestConversionPath = function (fromId, toId) {
+    var paths = converter.getConversionPaths(fromId, toId)
+    var chosenPath = null
+    for (var i = 0; i < paths.length; i++) {
+      if (chosenPath === null || paths[i].length < chosenPath.length) {
+        chosenPath = paths[i]
       }
     }
-    
-    return chosenPath;
+
+    return chosenPath
   }
-  
-  converter.getConversionIdConnections = function(){
-    var connections = [];
-    
-    var f = converter.formats;
-    var i, j;
-    for(i=0; i<f.length; i++){
-      for(j=0; j<f[i].converters.length; j++){
-        connections.push( [f[i].id, f[i].converters[j].id] );
+
+  converter.getConversionIdConnections = function () {
+    var connections = []
+
+    var f = converter.formats
+    var i, j
+    for (i = 0; i < f.length; i++) {
+      for (j = 0; j < f[i].converters.length; j++) {
+        connections.push([f[i].id, f[i].converters[j].id])
       }
     }
-    
-    return connections;
+
+    return connections
   }
-  
+
   /**
    * 1. Never go back to same node
    * 2. Max depth needed in case many paths likely.
    */
-  converter.getConversionPaths = function(fromId, toId){
-    var connections = converter.getConversionIdConnections();
-    var paths = converter.getPaths(fromId, toId, connections, maxDepth);
-    
-    return paths;
+  converter.getConversionPaths = function (fromId, toId) {
+    var connections = converter.getConversionIdConnections()
+    var maxDepth = 20
+    var paths = converter.getPaths(fromId, toId, connections, maxDepth)
+
+    return paths
   }
-  
+
   /**
    * fromNode AND toNode ARE ABSTRACT NODES. ONLY EQUALITY CHECK IMPORTANT.
    * connections: [[nodeA, nodeB], ...]
    */
-  converter.getPaths = function(fromNode, toNode, connections, maxDepth){
-    
-    //Create objects
-    var objList = [];
-    loopObject(connections, function(obj, key, val){
-      if(objList.indexOf(val) < 0){objList.push(val);}
-    });
-    
-    //??
+  converter.getPaths = function (fromNode, toNode, connections, maxDepth) {
+    console.log('getPaths', fromNode, toNode, connections, maxDepth) // Should implement maxDepth
+    // Create objects
+    var objList = []
+    ObjectTraverser.loopObject(connections, function (obj, key, val) {
+      if (objList.indexOf(val) < 0) {
+        objList.push(val)
+      }
+    })
+
+    // ??
   }
-  
-  converter.setup(formats, settings);
-  
-  return converter;
+
+  converter.setup(formats, settings)
+
+  return converter
 }
+
+module.exports = FormatConverter
